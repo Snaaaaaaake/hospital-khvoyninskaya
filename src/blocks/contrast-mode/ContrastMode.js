0633a6190d4284a66bbf775fc105106f0e0ab12a
Snaaaaaaake <kissmyshinyass@mail.ru>
2020-06-17 17:37:32 +0300

    html темплейты страницы собираются через вебпак

--- src/blocks/contrast-mode/ContrastMode.js
+++ src/blocks/contrast-mode/ContrastMode.js
@@ -2,65 +2,26 @@ import getCookie from '../../js/utils/getCookie';
 import setCookie from '../../js/utils/setCookie';
 
 export default class ContrastMode {
-  constructor() {
-    this._element = document.createElement('div');
-    this._element.classList.add('contrast-mode');
-    this._element.classList.add('element-disabled');
-    this._element.insertAdjacentHTML('afterbegin', `
-      <div class="contrast-mode__item">
-        <div class="contrast-mode__item-text">Размер<br>шрифта</div>
-        <div class="contrast-mode__item-content">
-          <div class="contrast-mode__item-content-font contrast-mode__item-content-font_small" title="Маленький шрифт">A</div>
-          <div class="contrast-mode__item-content-font contrast-mode__item-content-font_medium" title="Обычный шрифт">A</div>
-          <div class="contrast-mode__item-content-font contrast-mode__item-content-font_large" title="Большой шрифт">A</div>
-        </div>
-      </div>
-      <div class="contrast-mode__item">
-        <div class="contrast-mode__item-text">Цвет<br>сайта</div>
-        <div class="contrast-mode__item-content">
-          <div class="contrast-mode__item-cube contrast-mode__item-cube-button-white" title="Стандартный стиль"></div>
-          <div class="contrast-mode__item-cube contrast-mode__item-cube-button-black contrast-mode__item-cube_dark" title="Контрастный стиль"></div>
-        </div>
-      </div>
-      <div class="contrast-mode__item">
-        <div class="contrast-mode__item-text">Изображения</div>
-        <div class="contrast-mode__item-content contrast-mode__item-content-switch-off">
-          <div class="contrast-mode__item-switch contrast-mode__item-cube_dark"></div>
-          <div class="contrast-mode__item-switch contrast-mode__item-switch-button contrast-mode__item-switch-off">ВЫКЛ</div>
-        </div>
-        <div class="contrast-mode__item-content contrast-mode__item-content-switch-on element-disabled">
-          <div class="contrast-mode__item-switch contrast-mode__item-switch-button contrast-mode__item-switch-on">ВКЛ</div>
-          <div class="contrast-mode__item-switch contrast-mode__item-cube_dark"></div>
-        </div>
-      </div>
-      <div class="contrast-mode__item">
-        <div class="contrast-mode__item-content">
-          <button class="contrast-mode__item-text contrast-mode__item-content-big-button contrast-mode__item-content-big-button-hide" title="Скрыть меню">Скрыть<br>меню</button>
-        </div>
-      </div>
-    `);
-  }
-
   render(parentElement, openButton, additionalListener) {
     // Вешаем обработчики
     // Размер шрифта
-    this._element.querySelector('.contrast-mode__item-content-font_small').addEventListener('click', () => {
+    document.querySelector('.contrast-mode__item-content-font_small').addEventListener('click', () => {
       document.body.parentNode.classList.add('small');
       document.body.parentNode.classList.remove('large');
       setCookie('font', 'small');
     });
-    this._element.querySelector('.contrast-mode__item-content-font_large').addEventListener('click', () => {
+    document.querySelector('.contrast-mode__item-content-font_large').addEventListener('click', () => {
       document.body.parentNode.classList.remove('small');
       document.body.parentNode.classList.add('large');
       setCookie('font', 'large');
     });
-    this._element.querySelector('.contrast-mode__item-content-font_medium').addEventListener('click', () => {
+    document.querySelector('.contrast-mode__item-content-font_medium').addEventListener('click', () => {
       document.body.parentNode.classList.remove('small');
       document.body.parentNode.classList.remove('large');
       setCookie('font', 'medium');
     });
     // Выкл изображения
-    this._element.querySelector('.contrast-mode__item-switch-off').addEventListener('click', () => {
+    document.querySelector('.contrast-mode__item-switch-off').addEventListener('click', () => {
       document.querySelectorAll('.element-with-backgroud').forEach((elem) => {
         elem.classList.add('element-background-disabled');
       });
@@ -69,7 +30,7 @@ export default class ContrastMode {
       setCookie('img', 'disabled');
     });
     // Вкл изображения
-    this._element.querySelector('.contrast-mode__item-switch-on').addEventListener('click', () => {
+    document.querySelector('.contrast-mode__item-switch-on').addEventListener('click', () => {
       document.querySelectorAll('.element-with-backgroud').forEach((elem) => {
         elem.classList.remove('element-background-disabled');
       });
@@ -78,29 +39,26 @@ export default class ContrastMode {
       setCookie('img', 'enabled');
     });
     // Скрыть меню
-    this._element.querySelector('.contrast-mode__item-content-big-button-hide').addEventListener('click', () => {
-      this._element.classList.toggle('element-disabled');
+    document.querySelector('.contrast-mode__item-content-big-button-hide').addEventListener('click', () => {
+      document.querySelector('.contrast-mode').classList.toggle('element-disabled');
     });
     // Цвет
-    this._element.querySelector('.contrast-mode__item-cube-button-white').addEventListener('click', () => {
+    document.querySelector('.contrast-mode__item-cube-button-white').addEventListener('click', () => {
       document.body.parentNode.classList.remove('contrast');
       setCookie('mode', 'normal');
     });
-    this._element.querySelector('.contrast-mode__item-cube-button-black').addEventListener('click', () => {
+    document.querySelector('.contrast-mode__item-cube-button-black').addEventListener('click', () => {
       document.body.parentNode.classList.add('contrast');
       setCookie('mode', 'contrast');
     });
     // Показать меню
     openButton.addEventListener('click', () => {
-      this._element.classList.toggle('element-disabled');
+      document.querySelector('.contrast-mode').classList.toggle('element-disabled');
       if (window.screen.width <= 424 && additionalListener) {
         // При малом разрешении чтобы открыть меню доп возможностей, основное меню надо скрыть
         additionalListener();
       }
     });
-
-    // Добавляем элемент в документ
-    parentElement.appendChild(this._element);
   }
 
   // Специальные возможности применение
--- src/blocks/footer/Footer.js
+++ src/blocks/footer/Footer.js
@@ -1,69 +1,7 @@
 import spoilerToggleListener from '../../js/utils/spoilerToggleListener';
 
 export default class Footer {
-  constructor() {
-    this._element = document.createElement('div');
-    this._element.classList.add('footer');
-    this._element.classList.add('element-gray');
-    this._element.insertAdjacentHTML('afterbegin', `
-      <h5 class="footer__title">Хвойнинская ЦРБ</h5>
-      <nav class="footer__menu">
-        <ul class="list list_unmarked footer__container footer__list-item-container">
-          <li class="list__item footer__list-item-container">
-            <p class="list__title footer__list-title">КОНТАКТЫ</p>
-            <ul class="footer__container-list list list_unmarked">
-              <li class="list__item">
-                <a href="/contacts" class="list__link">Контакты больницы</a>
-              </li>
-              <li class="list__item">
-                <a href="/vacancy" class="list__link">Вакансии</a>
-              </li>
-            </ul>
-          </li>
-          <li class="list__item footer__list-item-container">
-            <p class="list__title footer__list-title">ПАЦИЕНТАМ</p>
-            <ul class="footer__container-list list list_unmarked">
-              <li class="list__item">
-                <a href="/shedule" class="list__link">Расписание</a>
-              </li>
-              <li class="list__item">
-                <a href="/activities" class="list__link">Основные виды деятельности</a>
-              </li>
-              <li class="list__item">
-                <a href="/prophylaxy" class="list__link">Профилактика</a>
-              </li>
-              <li class="list__item">
-                <a href="#" class="list__link">ГОСУСЛУГИ запись на прием</a>
-              </li>
-              <li class="list__item">
-                <a href="/writetous" class="list__link">Написать нам</a>
-              </li>
-              <li class="list__item">
-                <a href="#" class="list__link">Прочая информация</a>
-              </li>
-            </ul>
-          </li>
-          <li class="list__item footer__list-item-container">
-            <a href="/paidservices" class="list__title list__item footer__list-title">ПЛАТНЫЕ УСЛУГИ</a>
-          </li>
-          <li class="list__item footer__list-item-container">
-            <a href="/about" class="list__title list__item footer__list-title">О НАС</a>
-          </li>
-          <li class="list__item footer__list-item-container">
-            <p class="list__title list__item footer__list-title">ПРОТИВО&shy;ДЕЙСТВИЕ КОРРУПЦИИ</p>
-            <ul class="footer__container-list list list_unmarked">
-              <li class="list__item">
-                <a href="/corruption" class="list__link">Комиссия по противодействию коррупции</a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </nav>
-    `);
-  }
-
-  render(parentElement) {
-    this._element.querySelectorAll('.footer__list-title').forEach(spoilerToggleListener('footer__list-title_opened'));
-    parentElement.appendChild(this._element);
+  render() {
+    document.querySelectorAll('.footer__list-title').forEach(spoilerToggleListener('footer__list-title_opened'));
   }
 }
